87722378d0c08cf5a29e000ec5bdf0b482739f34
Ильяс <i.almakaev@gmail.com>
2017-08-23 12:38:45 +0700

    Initial Commit

---
+++ Patterns_20_Proxy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Patterns_20_Proxy
+//
+//  Created by Ильяс on 23.08.17.
+//  Copyright © 2017 Алмакаев Ильяс. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
